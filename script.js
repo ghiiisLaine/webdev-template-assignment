218b804f5b1dc9bd3347452616c97a12232e5975
ghiiisLaine <g.ghislainediolola@gmail.com>
2026-08-20 19:10:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Add your own interactivity here.
 // Example: log a greeting to the console.
 console.log("Hello from script.js!");
+
+<script>
+  document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("answer").textContent =
+      "I want to mix software development with my art";
+  });
+      
