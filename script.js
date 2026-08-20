24e5db6b1d0d083d0f562aee41413c692e37f0e2
ghiiisLaine <g.ghislainediolola@gmail.com>
2026-08-20 22:23:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 // Example: log a greeting to the console.
 console.log("Hello from script.js!");
 
-// Header button
-document.getElementById("myButton").addEventListener("click", function() {
-  document.getElementById("answer").textContent = "It's practical :p";
-});
+document.addEventListener("DOMContentLoaded", function() {
+  // First button
+  document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("answer").textContent = "It's practical :p";
+  });
 
-// Body button
-document.getElementById("HAir").addEventListener("click", function() {
-  document.getElementById("answerColor").textContent = "Idk lol";
+  // Second button
+  document.getElementById("HAir").addEventListener("click", function() {
+    document.getElementById("answerColor").textContent = "Idk lol";
+  });
 });
 
 
