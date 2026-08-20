ef15b70ffdbae3edd96aca95507c70390d351057
ghiiisLaine <g.ghislainediolola@gmail.com>
2026-08-20 19:12:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 // Example: log a greeting to the console.
 console.log("Hello from script.js!");
 
-<script>
-  document.getElementById("myButton").addEventListener("click", function() {
-    document.getElementById("answer").textContent =
-      "I want to mix software development with my art";
+
   });
       
